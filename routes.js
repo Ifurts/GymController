a7bfbb39416371bab690ac94bb21d0571a0708a0
Ivandro Furtado <ivandro.furtado@hotmail.com>
2020-08-23 21:17:40 -0300

    Fisrt commit

---
+++ routes.js
@@ -0,0 +1,18 @@
+const { Module } = require("module")
+const express = require('express')
+const routes = express.Router()
+
+routes.get('/', function(req, res){
+    return res.redirect("/instructors")
+})
+
+routes.get('/instructors', function(req, res){
+    return res.render("instructors/index")
+})
+
+routes.get('/students', function(req, res){
+    return res.render("students")
+})
+
+
+module.exports = routes
